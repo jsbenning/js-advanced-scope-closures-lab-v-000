c505ef1ada779859637b4125919a3cf1906a6617
Josef Benninghoff <jsbenning4@gmail.com>
2019-01-28 22:44:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function produceDrivingRange(blockRange){
   this.blockRange = blockRange;
-  return function(start, end){ 
+  return function(start, end){
+    let p1 = 
     if (Math.abs(start - end) > blockRange) {
       return false;
     } else {
